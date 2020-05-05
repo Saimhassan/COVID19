aedddf666aa790f436adbd6103375cdf037cf87e
Saim <saimh355@gmail.com>
2020-05-05 16:28:57 +0500

    COVID 19 ANDROID APP

--- app/src/main/java/com/example/covid19app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/covid19app/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.covid19app.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +14,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.covid19app.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeFragment extends Fragment {
 
@@ -38,7 +46,27 @@ public class HomeFragment extends Fragment {
 
     private void getData() {
         RequestQueue queue = Volley.newRequestQueue(getActivity());
-        String url = "https://corona.lmao.ninja/all";
+        String url = "https://corona.lmao.ninja/v2/all";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressBar.setVisibility(View.GONE);
+                try {
+                    JSONObject jsonObject = new JSONObject(response.toString());
+                    tvTotalConfirmed.setText(jsonObject.getString("cases"));
+                    tvTotalDeaths.setText(jsonObject.getString("deaths"));
+                    tvTotalRecovered.setText(jsonObject.getString("recovered"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                 progressBar.setVisibility(View.GONE);
+                Log.d("Error Response",error.toString());
+            }
+    });
 
     }
 }
